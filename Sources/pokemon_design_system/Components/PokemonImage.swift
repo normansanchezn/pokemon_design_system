cc083ff71f29f4997688ce60710aabc53816ddc4
Norman Sanchez <normansanchezn@gmail.com>
2026-03-25 10:38:38 -0600

    [✨] Get pokemon list

--- Sources/pokemon_design_system/Components/PokemonImage.swift
+++ Sources/pokemon_design_system/Components/PokemonImage.swift
@@ -9,19 +9,30 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 public struct PokemonImage: View {
-    
     public let pokeUrl: String
+
     public init(pokeUrl: String) {
         self.pokeUrl = pokeUrl
     }
-    
+
     public var body: some View {
         WebImage(
             url: URL(
                 string: pokeUrl
             )
-        ) { image in
-            image.image?.resizable()
+        ) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            case .failure:
+                Color.clear
+            case .empty:
+                ProgressView()
+            @unknown default:
+                Color.clear
+            }
         }
         .frame(width: 100, height: 100)
     }
--- Sources/pokemon_design_system/Extensions/View+Ext.swift
+++ Sources/pokemon_design_system/Extensions/View+Ext.swift
@@ -11,6 +11,18 @@ import pokemon_shared
 public extension View {
     func resolvedColorByType(_ type: PokemonType) -> Color {
         switch type {
+        case .normal:
+            return Color.gray.opacity(0.4)
+        case .fighting:
+            return Color.red.opacity(0.55)
+        case .flying:
+            return Color.cyan.opacity(0.4)
+        case .poison:
+            return Color.purple.opacity(0.4)
+        case .ground:
+            return Color.brown.opacity(0.45)
+        case .rock:
+            return Color.orange.opacity(0.35)
         case .bug:
             return Color.blue.opacity(0.4)
         case .dark:
@@ -19,16 +31,22 @@ public extension View {
             return Color.orange.opacity(0.4)
         case .fairy:
             return Color.pink.opacity(0.4)
-        case .weed:
+        case .grass:
             return Color.green.opacity(0.4)
-        case .poison:
-            return Color.purple.opacity(0.4)
         case .fire:
             return Color.red.opacity(0.4)
         case .water:
             return Color.blue.opacity(0.4)
-        default:
-            return Color.black.opacity(0.4)
+        case .electric:
+            return Color.yellow.opacity(0.5)
+        case .psychic:
+            return Color.pink.opacity(0.55)
+        case .ice:
+            return Color.cyan.opacity(0.55)
+        case .ghost:
+            return Color.indigo.opacity(0.45)
+        case .steel:
+            return Color.gray.opacity(0.55)
         }
     }
 }
@@ -36,20 +54,42 @@ public extension View {
 public extension View {
     func resolvedType(type: PokemonType) -> String {
         switch type {
-        case .weed:
+        case .normal:
+            return "Normal"
+        case .fighting:
+            return "Fighting"
+        case .flying:
+            return "Flying"
+        case .poison:
+            return "Poison"
+        case .ground:
+            return "Ground"
+        case .rock:
+            return "Rock"
+        case .grass:
             return "Grass"
         case .fire:
             return "Fire"
         case .water:
             return "Water"
-        case .earth:
-            return "Ground"
-        case .poison:
-            return "Poison"
+        case .electric:
+            return "Electric"
+        case .psychic:
+            return "Psychic"
+        case .ice:
+            return "Ice"
         case .dragon:
             return "Dragon"
-        default:
-            return ""
+        case .dark:
+            return "Dark"
+        case .bug:
+            return "Bug"
+        case .ghost:
+            return "Ghost"
+        case .steel:
+            return "Steel"
+        case .fairy:
+            return "Fairy"
         }
     }
 }
